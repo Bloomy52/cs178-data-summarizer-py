213732c4d7635117d9c504ba1547b6e4adbc3991
Louie Bloomberg <basis-gyros8g@icloud.com>
2026-05-04 14:06:31 -0500

    Added Amazon Bedrock code

--- lambda.py
+++ lambda.py
@@ -19,37 +19,63 @@ def lambda_handler(event, context):
     # ── Step 1: Extract the bucket name and filename from the trigger event ───
     # When S3 triggers Lambda, the event contains a 'Records' list.
     # Each record describes one file that was uploaded.
+    s3 = boto3.client("s3")
     record = event["Records"][0]
     source_bucket = record["s3"]["bucket"]["name"]
     filename = record["s3"]["object"]["key"]
 
     print(f"Triggered by upload: s3://{source_bucket}/{filename}")
 
-    # ── Step 2: Download the uploaded image from S3 into memory ──────────────
-    s3 = boto3.client("s3")
-    response = s3.get_object(Bucket=source_bucket, Key=filename)
-    csv_data = response["Body"].read()
+    # ── Step 2: Open the prompt.txt file ──────────────
+    with open("prompt.txt", "r") as infile:
+       prompt = infile.read()
+
+    # ── Step 3: Open the csv file from S3 and upload to Bedrock ──────────────
+    bedrock = boto3.client("bedrock-runtime", region_name="us-east-1")
+
+    # Open the Uploaded CSV file
+    response = bedrock.converse(
+        modelId="global.amazon.nova-2-lite-v1:0",
+        messages=[{
+            "role": "user",
+            "content": [
+                {"document": {
+                    "format": "csv",
+                    "name": filename,
+                    "source": {"s3": f"s3://{source_bucket}/{filename}"}
+                }
+                },
+                {"text": prompt}
+            ]
+        }]
+    )
+
+    # ── Step 4: Write the summary to a new text file  ─────────────
+    output = response["output"]["message"]["content"][0]["text"]
+
+    with open(f"./summary_temps/{filename}.txt", "w") as outfile:
+        outfile.write(output)
 
 
-
-    # ── Step 4: Save the txt summary back into a BytesIO buffer ─────────────
+    # ── Step 5: Save the txt summary back into a BytesIO buffer ─────────────
     buffer = BytesIO()
     file_format = "txt"
+    txt_filename = f"{filename}.txt"
 
 
-    # ── Step 5: Upload the result to the processed bucket ─────────────────────
+    # ── Step 6: Upload the result to the processed bucket ─────────────────────
     s3.put_object(
         Bucket=creds.S3_SUMMARY_BUCKET,
-        Key=filename,           # same filename, different bucket
+        Key=txt_filename,           # same filename, different bucket
         Body=buffer,
         ContentType=f"txt/{file_format.lower}",
     )
 
-    print(f"Processed image saved to: s3://{creds.S3_SUMMARY_BUCKET}/{filename}")
+    print(f"Processed image saved to: s3://{creds.S3_SUMMARY_BUCKET}/{txt_filename}")
 
     return {
         "statusCode": 200,
-        "body": f"Flipped {filename} and saved to {creds.S3_SUMMARY_BUCKET}"
+        "body": f"Summarized {filename} and saved to {creds.S3_SUMMARY_BUCKET}"
     }
 
 
